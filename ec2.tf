6128afc2825bf7877683e5def9966ab6a8c008ee
Smihah <smihah270@yahoo.com>
2023-07-20 11:28:12 +0100

    added ingress rule port 80

--- ec2.tf
+++ ec2.tf
@@ -13,6 +13,13 @@ resource "aws_security_group" "pub-ec2-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+      description = "http traffic from internet"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 
   egress {
     from_port   = 0
