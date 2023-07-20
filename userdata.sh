d4c471fd50f97b5b9f5ac1ed5325533ac1a7ce8c
Smihah <smihah270@yahoo.com>
2023-07-20 14:24:44 +0100

    modified user data

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,16 @@
-#!bin/bash
+#!/bin/bash
 yum update -y
 yum install -y httpd
-git https://github.com/gabrielecirulli/2048.git
-cp -R 2048/* /var/www/html/
+yum install -y git
+
+# Clone the 2048 repository
+git clone https://github.com/gabrielecirulli/2048.git
+
+# Move the cloned files to the appropriate directory
+mv 2048/* /var/www/html/
+
+# Start and enable the Apache web server
 systemctl start httpd && systemctl enable httpd
+
+
 
